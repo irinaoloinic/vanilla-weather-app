5b515a4ff07e230567f4df9221f67b406ec4fb92
irinaoloinic <133090430+irinaoloinic@users.noreply.github.com>
2023-05-15 22:17:36 +0200

    Integrated weather API

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+let apiKey = "37f6a94ba46fa4c11df96b5390cdt21o";
+let city = "Lisbon";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+console.log(apiUrl);
 
+function showTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElemt = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElemt.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+axios.get(apiUrl).then(showTemperature);
